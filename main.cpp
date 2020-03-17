7b061391f870d0ed302195e5a4e0cfb9a88b25c2
Nicholas Mosier <nh.mosier@gmail.com>
2020-03-17 11:26:25 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <unistd.h>
+#include <iostream>
+
+#include <LIEF/LIEF.hpp>
+
+void usage(FILE *f, int argc, char *argv[]) {
+   fprintf(f,
+           "usage: %1$s <macho>\n"              \
+           "       %1$s -h\n",
+           argv[0]
+           );
+}
+
+int main(int argc, char *argv[]) {
+   const char *optstring = "h";
+   int optchar;
+   while ((optchar = getopt(argc, argv, optstring)) >= 0) {
+      switch (optchar) {
+      case 'h':
+         usage(stdout, argc, argv);
+         return 0;
+      case '?':
+         usage(stderr, argc, argv);
+         return 1;
+      }
+   }
+
+   if (argc - optind != 1) {
+      usage(stderr, argc, argv);
+      return 1;
+   }
+
+   const char *in_path = argv[optind++];
+   
+   std::unique_ptr<LIEF::MachO::FatBinary> macho = LIEF::MachO::Parser::parse(in_path);
+   std::cout << *macho << std::endl;
+   
+   return 0;
+}
+
