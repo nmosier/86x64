c26684e1b3d5bcfe5ce3ee6a21d7f12d28ae84a4
Nicholas Mosier <nh.mosier@gmail.com>
2020-06-13 17:49:21 -0700

    cmmit

--- c++/macho.cc
+++ c++/macho.cc
@@ -2,34 +2,43 @@
 
 #include <fcntl.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/errno.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 
 #include "macho.hh"
 #include "util.hh"
 
-int main(void) {
-   
+int main(int argc, char *argv[]) {
+   MachO::Image img(argv[0]);
+   int i = img.at<int>(0);
    return 0;
 }
 
 namespace MachO {
    
-   MachO::MachO(const char *path) {
+   Image::Image(const char *path) {
       if ((fd = open(path, O_RDWR)) < 0) {
          throw cerror("open");
       }
 
       struct stat st;
       if (fstat(fd, &st) < 0) {
+         close(fd);
          throw cerror("fstat");
       }
-
+      filesize = st.st_size;
       
+      if ((img = mmap(NULL, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) == NULL) {
+         close(fd);
+         throw cerror("mmap");
+      }
    }
    
-   MachO::~MachO() {
-      
+   Image::~Image() {
+      munmap(img, filesize);
+      close(fd);
    }
 
 }
--- c++/macho.hh
+++ c++/macho.hh
@@ -14,73 +14,23 @@ namespace MachO {
    template <Bits bits>
    using macho_size_t = macho_addr_t<bits>;
 
-#if 0
    class Image {
    public:
-      Image(const char *path, int oflag) {
-         if ((fd = open(path, O_RDONLY)))
-      }
-      
+      Image(const char *path);
+      ~Image();
+
+      template <typename T>
+      const T& at(size_t index) const { return * (const T *) ((const char *) img + index); }
+
+      template <typename T>
+      T& at(size_t index) { return * (T *) ((char *) img + index); }
       
    private:
       int fd;
-      void *map;
-      // size_t size;
+      size_t filesize;
+      void *img;
    };
    
-   class MachO {
-   public:
-      using magic_t = uint32_t;
-      
-      virtual magic_t magic() const = 0;
-   protected:
-      MachO() {}
-   };
-
-   class Fat: public MachO {
-   public:
-      
-   private:
-      
-   };
-
-   template <Bits bits>
-   class Archive: public MachO {
-   public:
-      using filetype_t = uint32_t;
-      using flags_t = uint32_t;
-      
-      virtual magic_t magic() const override { return magic_; }
-      cpu_type_t cputype() const { return cputype_; }
-      cpu_subtype_t cpusubtype() const { return cpusubtype_; }
-      filetype_t filetype() const { return filetype_; }
-      flags_t flags() const { return flags_; }
-
-   protected:
-      magic_t magic_;
-      cpu_type_t cputype_;
-      cpu_subtype_t cpusubtype_;
-      filetype_t filetype_;
-      flags_t flags_;
-
-      Archive(magic_t magic, cpu_type_t cputype, cpu_subtype_t cpusubtype, filetype_t filetype,
-              flags_t flags): magic_(magic), cputype_(cputype), cpusubtype_(cpusubtype),
-                              filetype_(filetype), flags_(flags) {}
-   };
-
-   template <Bits bits>
-   class MachHeader {
-   public:
-      uint32_t magic() const { return header.magic; }
-      
-   private:
-      using Header = select_type<bits, struct mach_header, struct mach_header_64>;
-      Header header;
-   };
-
-#endif
-
-
    class MachO {
    public:
       MachO(const char *path);
@@ -88,6 +38,7 @@ namespace MachO {
       
    private:
       int fd;
+      size_t filesize;
       void *img;
    };
    
