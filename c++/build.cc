42250088c695b10e8aebad5c791b1bbe7b4478b9
Nicholas Mosier <nh.mosier@gmail.com>
2020-06-25 11:51:08 -0700

    ASa

---
+++ c++/build.cc
@@ -0,0 +1,25 @@
+#include "build.hh"
+#include "util.hh"
+
+namespace MachO {
+
+   template <Bits bits>
+   std::size_t BuildEnv<bits>::allocate(std::size_t size) {
+      // loc.offset = align<bits>(loc.offset);
+      std::size_t r_offset = loc.offset;
+      loc.offset += size;
+      return r_offset;
+   }
+   
+   template <Bits bits>
+   void BuildEnv<bits>::allocate(std::size_t size, Location& out_loc) {
+      
+      out_loc.offset = allocate(size);
+      out_loc.vmaddr = loc.vmaddr;
+      loc.vmaddr += size;
+   }
+
+   template class BuildEnv<Bits::M32>;
+   template class BuildEnv<Bits::M64>;
+   
+}
