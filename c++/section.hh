19745b7f482cb5a2d364763e4345db7ac170328d
Nicholas Mosier <nh.mosier@gmail.com>
2020-06-26 11:10:49 -0700

    fixed memcpy bug

---
+++ c++/section.hh
@@ -0,0 +1,236 @@
+#pragma once
+
+#include <list>
+#include <vector>
+#include <mach-o/loader.h>
+
+#include "macho-fwd.hh"
+#include "util.hh"
+#include "loc.hh"
+#include "parse.hh"
+#include "build.hh"
+#include "image.hh"
+
+namespace MachO {
+
+   template <Bits bits> class ZeroBlob;
+   template <Bits bits> class LazySymbolPointer;
+   
+   template <Bits bits>
+   class Section {
+   public:
+      using section_t = select_type<bits, section, section_64>;
+
+      section_t sect;
+
+      std::string name() const;
+      Location loc() const { return Location(sect.offset, sect.addr); }
+      void loc(const Location& loc) { sect.offset = loc.offset; sect.addr = loc.vmaddr; }
+
+      virtual ~Section() {}
+      
+      static std::size_t size() { return sizeof(section_t); }
+      static Section<bits> *Parse(const Image& img, std::size_t offset, ParseEnv<bits>& env);
+      virtual void Build(BuildEnv<bits>& env);
+      virtual std::size_t content_size() const = 0;
+      void Emit(Image& img, std::size_t offset) const;
+
+   protected:
+      Section(const Image& img, std::size_t offset): sect(img.at<section_t>(offset)) {}
+      virtual void Build_content(BuildEnv<bits>& env) = 0;
+      virtual void Emit_content(Image& img, std::size_t offset) const = 0;
+   };
+
+   template <Bits bits, class Elem>
+   class SectionT: public Section<bits> {
+   public:
+      using Content = std::list<Elem *>;
+      Content content;
+
+      ~SectionT();
+      virtual std::size_t content_size() const override;
+      
+   protected:
+      typedef Elem *(*Parser)(const Image&, const Location&, ParseEnv<bits>&);
+      SectionT(const Image& img, std::size_t offset, ParseEnv<bits>& env, Parser parser);
+      SectionT(const Image& img, std::size_t offset, ParseEnv<bits>& env):
+         SectionT(img, offset, env, [](const Image& img, const Location& loc, ParseEnv<bits>& env) {
+                                       return Elem::Parse(img, loc, env);
+                                    }) {}
+      virtual void Build_content(BuildEnv<bits>& env) override;
+      virtual void Emit_content(Image& img, std::size_t offset) const override;
+   };
+
+   template <Bits bits>
+   class TextSection: public SectionT<bits, SectionBlob<bits>> {
+   public:
+      template <typename... Args>
+      static TextSection<bits> *Parse(Args&&... args) { return new TextSection(args...); }
+   private:
+      static SectionBlob<bits> *BlobParser(const Image& img, const Location& loc,
+                                           ParseEnv<bits>& env);
+      TextSection(const Image& img, std::size_t offset, ParseEnv<bits>& env):
+         SectionT<bits, SectionBlob<bits>>(img, offset, env, BlobParser) {}
+   };
+
+   template <Bits bits>
+   class ZerofillSection: public SectionT<bits, ZeroBlob<bits>> {
+   public:
+      template <typename... Args>
+      static ZerofillSection<bits> *Parse(Args&&... args) { return new ZerofillSection(args...); }
+      virtual void Build(BuildEnv<bits>& env) override;
+   private:
+      ZerofillSection(const Image& img, std::size_t offset, ParseEnv<bits>& env):
+         SectionT<bits, ZeroBlob<bits>>(img, offset, env) {}
+      virtual void Emit_content(Image& img, std::size_t offset) const override {}
+   };
+
+   template <Bits bits>
+   class SectionBlob {
+   public:
+      Segment<bits> *segment; /*!< containing segment */
+      Location loc; /*!< Post-build location */
+      
+      virtual std::size_t size() const = 0;
+      virtual ~SectionBlob() {}
+      void Build(BuildEnv<bits>& env);
+      virtual void Emit(Image& img, std::size_t offset) const = 0;
+      
+   protected:
+      SectionBlob(const Location& loc, ParseEnv<bits>& env);
+   };
+
+   template <Bits bits>
+   class DataBlob: public SectionBlob<bits> {
+   public:
+      uint8_t data;
+      virtual std::size_t size() const override { return 1; }
+      virtual void Emit(Image& img, std::size_t offset) const override;
+      template <typename... Args>
+      static DataBlob<bits> *Parse(Args&&... args) { return new DataBlob(args...); }
+      
+   private:
+      DataBlob(const Image& img, const Location& loc, ParseEnv<bits>& env):
+         SectionBlob<bits>(loc, env), data(img.at<uint8_t>(loc.offset)) {}
+   };
+
+   template <Bits bits>
+   class ZeroBlob: public SectionBlob<bits> {
+   public:
+      virtual std::size_t size() const override { return 1; }
+      virtual void Emit(Image& img, std::size_t offset) const override {}
+
+      template <typename... Args>
+      static ZeroBlob<bits> *Parse(Args&&... args) {
+         return new ZeroBlob(std::forward<Args>(args)...);
+      }
+      
+   private:
+      ZeroBlob(const Image& img, const Location& loc, ParseEnv<bits>& env):
+         SectionBlob<bits>(loc, env) {}
+   };
+   
+   template <Bits bits>
+   class DataSection: public SectionT<bits, DataBlob<bits>> {
+   public:
+      template <typename... Args>
+      static DataSection<bits> *Parse(Args&&... args) {
+         return new DataSection(std::forward<Args>(args)...);
+      }
+   private:
+      DataSection(const Image& img, std::size_t offset, ParseEnv<bits>& env):
+         SectionT<bits, DataBlob<bits>>(img, offset, env) {}
+   };
+   
+   template <Bits bits>
+   class LazySymbolPointerSection: public SectionT<bits, LazySymbolPointer<bits>> {
+   public:
+      template <typename... Args>
+      static LazySymbolPointerSection<bits> *Parse(Args&&... args) {
+         return new LazySymbolPointerSection<bits>(args...);
+      }
+   private:
+      LazySymbolPointerSection(const Image& img, std::size_t offset, ParseEnv<bits>& env):
+         SectionT<bits, LazySymbolPointer<bits>>(img, offset, env) {}
+   };
+
+   template <Bits bits>
+   class SymbolPointer: public SectionBlob<bits> {
+   public:
+      using ptr_t = select_type<bits, uint32_t, uint64_t>;
+      
+      static std::size_t Size() { return sizeof(ptr_t); }
+      virtual std::size_t size() const override { return sizeof(ptr_t); }
+      virtual void Emit(Image& img, std::size_t offset) const override;
+      
+   protected:
+      template <typename... Args>
+      SymbolPointer(Args&&... args): SectionBlob<bits>(args...) {}
+      virtual ptr_t raw_data() const = 0;
+   };
+
+   template <Bits bits>
+   class LazySymbolPointer: public SymbolPointer<bits> {
+   public:
+      const Instruction<bits> *pointee; /*!< initial pointee */
+
+      template <typename... Args>
+      static LazySymbolPointer<bits> *Parse(Args&&... args) {
+         return new LazySymbolPointer(args...);
+      }
+      
+   private:
+      LazySymbolPointer(const Image& img, const Location& loc, ParseEnv<bits>& env);
+      virtual typename SymbolPointer<bits>::ptr_t raw_data() const override {
+         return pointee->loc.vmaddr;
+      }
+   };
+
+   template <Bits bits>
+   class NonLazySymbolPointer: public SymbolPointer<bits> {
+   public:
+      template <typename... Args>
+      static NonLazySymbolPointer<bits> *Parse(Args&&... args) {
+         return new NonLazySymbolPointer(args...);
+      }
+   private:
+      NonLazySymbolPointer(const Image& img, const Location& loc, ParseEnv<bits>& env):
+         SymbolPointer<bits>(loc, env) {}
+      virtual typename SymbolPointer<bits>::ptr_t raw_data() const override { return 0x0; }
+   };
+
+   template <Bits bits>
+   class NonLazySymbolPointerSection: public SectionT<bits, NonLazySymbolPointer<bits>> {
+   public:
+      template <typename... Args>
+      static NonLazySymbolPointerSection<bits> *Parse(Args&&... args) {
+         return new NonLazySymbolPointerSection(args...);
+      }
+      
+   private:
+      NonLazySymbolPointerSection(const Image& img, std::size_t offset, ParseEnv<bits>& env):
+         SectionT<bits, NonLazySymbolPointer<bits>>(img, offset, env) {}
+   };
+
+   template <Bits bits>
+   class Instruction: public SectionBlob<bits> {
+   public:
+      static const xed_state_t dstate;
+      
+      std::vector<uint8_t> instbuf;
+      xed_decoded_inst_t xedd;
+      unsigned memidx;
+      const SectionBlob<bits> *memdisp; /*!< memory displacement pointee */
+      const SectionBlob<bits> *brdisp;  /*!< branch displacement pointee */
+      
+      virtual std::size_t size() const override { return instbuf.size(); }
+      virtual void Emit(Image& img, std::size_t offset) const override;
+
+      template <typename... Args>
+      static Instruction<bits> *Parse(Args&&... args) { return new Instruction(args...); }
+      
+   private:
+      Instruction(const Image& img, const Location& loc, ParseEnv<bits>& env);
+   };   
+
+}
--- c++/segment.cc
+++ c++/segment.cc
@@ -40,169 +40,8 @@ namespace MachO {
    }   
 
    template <Bits bits>
-   Section<bits> *Section<bits>::Parse(const Image& img, std::size_t offset, ParseEnv<bits>& env) {
-      section_t sect = img.at<section_t>(offset);
-      uint32_t flags = sect.flags;
-
-      /* chcek whether contains instructions */
-      std::vector<std::string> text_sectnames = {SECT_TEXT, SECT_STUBS, SECT_STUB_HELPER};
-      for (const std::string& sectname : text_sectnames) {
-         if (sectname == sect.sectname) {
-            return TextSection<bits>::Parse(img, offset, env);
-         }
-      }
-      
-      if (flags == S_LAZY_SYMBOL_POINTERS) {
-         return LazySymbolPointerSection<bits>::Parse(img, offset, env);
-      } else if (flags == S_NON_LAZY_SYMBOL_POINTERS) {
-         return NonLazySymbolPointerSection<bits>::Parse(img, offset, env);
-      } else if (flags == S_REGULAR || flags == S_CSTRING_LITERALS) {
-         return DataSection<bits>::Parse(img, offset, env);
-      } else if ((flags & S_ZEROFILL)) {
-         return ZerofillSection<bits>::Parse(img, offset, env);
-      }
-
-      throw error("bad section flags (section %s)", sect.sectname);
-   }
-
-   template <Bits bits>
-   const xed_state_t Instruction<bits>::dstate =
-      {select_value(bits, XED_MACHINE_MODE_LEGACY_32, XED_MACHINE_MODE_LONG_64),
-       select_value(bits, XED_ADDRESS_WIDTH_32b, XED_ADDRESS_WIDTH_32b)
-      };
-
-   template <Bits bits>
-   Instruction<bits>::Instruction(const Image& img, const Location& loc, ParseEnv<bits>& env):
-      SectionBlob<bits>(loc, env), memdisp(nullptr), brdisp(nullptr)
-   {
-      xed_error_enum_t err;
-      
-      xed_decoded_inst_zero_set_mode(&xedd, &dstate);
-      xed_decoded_inst_set_input_chip(&xedd, XED_CHIP_INVALID);
-
-      if ((err = xed_decode(&xedd, &img.at<uint8_t>(loc.offset), img.size() - loc.offset)) !=
-          XED_ERROR_NONE) {
-         throw error("%s: offset 0x%x: xed_decode: %s", __FUNCTION__, loc.offset,
-                     xed_error_enum_t2str(err));
-      }
-      instbuf = std::vector<uint8_t>(&img.at<uint8_t>(loc.offset),
-                                     &img.at<uint8_t>(loc.offset + xed_decoded_inst_get_length(&xedd)));
-      const std::size_t refaddr = loc.vmaddr + xed_decoded_inst_get_length(&xedd);
-      
-      
-
-      xed_operand_values_t *operands = xed_decoded_inst_operands(&xedd);
-
-      /* Memory Accesses */
-      // const unsigned int memops = xed_operand_values_number_of_memory_operands(operands);
-      const unsigned int nops = xed_decoded_inst_noperands(&xedd);
-      if (xed_operand_values_has_memory_displacement(operands)) {
-         for (unsigned i = 0; i < nops; ++i) {
-            xed_reg_enum_t basereg  = xed_decoded_inst_get_base_reg(operands,  i);
-            xed_reg_enum_t indexreg = xed_decoded_inst_get_index_reg(operands, i);
-            if (basereg == select_value(bits, XED_REG_EIP, XED_REG_RIP) &&
-                indexreg == XED_REG_INVALID)
-               {
-                  if (memdisp) {
-                     fprintf(stderr,
-                             "warning: %s: duplicate memdisp at offset 0x%jx, vmaddr 0x%jx\n",
-                             __FUNCTION__, loc.offset, loc.vmaddr);
-                     abort();
-                  }
-
-                  memidx = i;
-                  
-                  /* get memory displacement & reference address */
-                  const ssize_t memdisp = xed_decoded_inst_get_memory_displacement(operands,
-                                                                                        i);
-                  const std::size_t targetaddr = refaddr + memdisp;
-
-                  /* resolve pointer */
-                  env.vmaddr_resolver.resolve(targetaddr, &this->memdisp);
-
-                  char pbuf[32];
-                  xed_print_info_t pinfo;
-                  xed_init_print_info(&pinfo);
-                  pinfo.blen = 32;
-                  pinfo.buf = pbuf;
-                  pinfo.p = &xedd;
-                  pinfo.runtime_address = loc.vmaddr;
-                  xed_format_generic(&pinfo);
-                  fprintf(stderr, "%s\n", pbuf);
-               }
-         }
-      }
-      
-      /* Relative Branches */
-      if (xed_operand_values_has_branch_displacement(operands)) {
-         const ssize_t brdisp = xed_decoded_inst_get_branch_displacement(operands);
-         const size_t targetaddr = refaddr + brdisp;
-
-         /* resolve */
-         env.vmaddr_resolver.resolve(targetaddr, &this->brdisp);
-      }
-   }
-
-   template <Bits bits>
-   SectionBlob<bits> *TextSection<bits>::BlobParser(const Image& img, const Location& loc,
-                                                    ParseEnv<bits>& env) {
-      try {
-         return Instruction<bits>::Parse(img, loc, env);
-      } catch (...) {
-         return DataBlob<bits>::Parse(img, loc, env);
-      }
-   }
-
-   template <Bits bits>
    std::size_t Segment<bits>::size() const {
       return sizeof(segment_command) + sections.size() * Section<bits>::size();
-   }
-
-   template <Bits bits>
-   SectionBlob<bits>::SectionBlob(const Location& loc, ParseEnv<bits>& env):
-      segment(env.current_segment)
-   {
-      assert(segment);
-      env.vmaddr_resolver.add(loc.vmaddr, this);
-      env.offset_resolver.add(loc.offset, this);
-   }
-
-   template <Bits bits>
-   LazySymbolPointer<bits>::LazySymbolPointer(const Image& img, const Location& loc,
-                                              ParseEnv<bits>& env):
-      SymbolPointer<bits>(loc, env)
-   {
-      using ptr_t = select_type<bits, uint32_t, uint64_t>;
-      
-      std::size_t targetaddr = img.at<ptr_t>(loc.offset);
-      env.vmaddr_resolver.resolve(targetaddr, (const SectionBlob<bits> **) &pointee);
-   }
-
-   template <Bits bits, class Elem>
-   SectionT<bits, Elem>::~SectionT() {
-      for (Elem *elem : content) {
-         delete elem;
-      }
-   }
-
-   template <Bits bits, class Elem>
-   SectionT<bits, Elem>::SectionT(const Image& img, std::size_t offset, ParseEnv<bits>& env,
-                                  Parser parser): Section<bits>(img, offset) {
-      const std::size_t begin = this->sect.offset;
-      const std::size_t end = begin + this->sect.size;
-      std::size_t it = begin;
-      std::size_t vmaddr = this->sect.addr;
-      while (it != end) {
-         Elem *elem = parser(img, Location(it, vmaddr), env);
-         content.push_back(elem);
-         it += elem->size();
-         vmaddr += elem->size();
-      }
-   }
-
-   template <Bits bits>
-   std::string Section<bits>::name() const {
-      return std::string(sect.sectname, strnlen(sect.sectname, sizeof(sect.sectname)));
    }
 
    template <Bits bits>
@@ -221,17 +60,6 @@ namespace MachO {
                                                           sizeof(segment_command.segname)));
    }
 
-#if 0
-   template <Bits bits>
-   std::size_t Segment<bits>::content_size() const {
-      std::size_t total_size = 0;
-      for (const Section<bits> *sect : sections) {
-         total_size += sect->content_size();
-      }
-      return total_size;
-   }
-#endif
-
    template <Bits bits>
    void Segment<bits>::Build(BuildEnv<bits>& env) {
       assert(env.loc.vmaddr % PAGESIZE == 0);
@@ -241,7 +69,6 @@ namespace MachO {
          return;
       }
 
-      
       /* set segment start location */
       if (strcmp(segment_command.segname, SEG_TEXT) == 0) {
          env.loc.vmaddr = align_up(env.loc.vmaddr, PAGESIZE);
@@ -280,55 +107,6 @@ namespace MachO {
       this->segment_command.vmsize = align_up<std::size_t>(this->segment_command.filesize, PAGESIZE);
    }
 
-#if 0
-   template <Bits bits>
-   std::size_t Segment_LINKEDIT<bits>::content_size() const {
-      std::size_t size = 0;
-      for (LinkeditCommand<bits> *linkedit : linkedits) {
-         size += linkedit->content_size();
-      }
-      return size;
-   }
-#endif
-   
-   template <Bits bits>
-   void SectionBlob<bits>::Build(BuildEnv<bits>& env) {
-      env.allocate(size(), loc);
-   }
-
-   template <Bits bits>
-   void Section<bits>::Build(BuildEnv<bits>& env) {
-      env.align(sect.align);
-      loc(env.loc);
-      Build_content(env);
-      sect.size = env.loc.offset - loc().offset;
-      fprintf(stderr, "[BUILD] segment %s, section %s @ offset 0x%zx, vmaddr 0x%zx\n", sect.segname,
-              sect.sectname, loc().offset, loc().vmaddr);
-   }
-
-   template <Bits bits>
-   void ZerofillSection<bits>::Build(BuildEnv<bits>& env) {
-      // env.align(this->sect.align);
-      this->sect.size = this->content_size();
-      this->loc(Location(0, env.loc.vmaddr));
-   }
-
-   template <Bits bits, class Elem>
-   void SectionT<bits, Elem>::Build_content(BuildEnv<bits>& env) {
-      for (Elem *elem : content) {
-         elem->Build(env);
-      }
-   }
-   
-   template <Bits bits, class Elem>
-   std::size_t SectionT<bits, Elem>::content_size() const {
-      std::size_t size = 0;
-      for (const Elem *elem : content) {
-         size += elem->size();
-      }
-      return align<bits>(size);
-   }
-
    template <Bits bits>
    void Segment<bits>::Build_PAGEZERO(BuildEnv<bits>& env) {
       assert(env.loc.vmaddr == 0);
@@ -351,78 +129,6 @@ namespace MachO {
 
       fprintf(stderr, "[EMIT] segment={name=%s,fileoff=0x%zx,filesize=0x%zx,vmaddr=0x%zx,vmsize=0x%zx}\n", segment_command.segname, (std::size_t) segment_command.fileoff, (size_t) segment_command.filesize, (size_t) segment_command.vmaddr, (size_t) segment_command.vmsize);
    }
-
-   template <Bits bits>
-   void Section<bits>::Emit(Image& img, std::size_t offset) const {
-      img.at<section_t>(offset) = sect;
-      Emit_content(img, sect.offset);
-   }
-   
-   template <Bits bits, class Elem>
-   void SectionT<bits, Elem>::Emit_content(Image& img, std::size_t offset) const {
-      for (const Elem *elem : content) {
-         elem->Emit(img, offset);
-         offset += elem->size();
-      }
-   }
-
-   template <Bits bits>
-   void DataBlob<bits>::Emit(Image& img, std::size_t offset) const {
-      img.at<uint8_t>(offset) = data;
-   }
-
-   template <Bits bits>
-   void SymbolPointer<bits>::Emit(Image& img, std::size_t offset) const {
-      img.at<ptr_t>(offset) = raw_data();
-   }
-
-   template <Bits bits>
-   void Instruction<bits>::Emit(Image& img, std::size_t offset) const {
-      /* patch instruction */
-      xed_decoded_inst_t xedd = this->xedd;
-      std::vector<uint8_t> instbuf = this->instbuf;
-      
-      if (memdisp) {
-         const unsigned width_bits =
-            xed_decoded_inst_get_memory_displacement_width_bits(&xedd, memidx);
-         const std::size_t disp = memdisp->loc.vmaddr -
-            (ssize_t) (this->loc.vmaddr + size());
-         xed_enc_displacement_t enc; // = {disp, width_bits};
-         enc.displacement = disp;
-         enc.displacement_bits = width_bits;
-         if (!xed_patch_disp(&xedd, &*instbuf.begin(), enc)) {
-            throw error("%s: xed_patch_disp: failed to patch instruction at offset 0x%zx, " \
-                        "vmaddr 0x%zx\n", __FUNCTION__, this->loc.offset, this->loc.vmaddr);
-         }
-      }
-      
-      if (brdisp) {
-         const unsigned width_bits = xed_decoded_inst_get_branch_displacement_width_bits(&xedd);
-         const std::size_t disp = brdisp->loc.vmaddr - (ssize_t) (this->loc.vmaddr + size());
-         xed_encoder_operand_t enc;
-         enc.u.brdisp = disp;
-         enc.width_bits = width_bits;
-         if (!xed_patch_relbr(&xedd, &*instbuf.begin(), enc)) {
-            throw error("%s: xed_patch_relbr: failed to patch instruction at offset 0x%zx, " \
-                        "vmaddr 0x%zx\n", __FUNCTION__, this->loc.offset, this->loc.vmaddr);
-         }
-      }
-         
-      /* emit instruction bytes */
-      memcpy(&img.at<uint8_t>(offset), &*instbuf.begin(), instbuf.size());
-   }
-
-#if 0
-   template <Bits bits>
-   void Segment_LINKEDIT<bits>::Build(BuildEnv<bits>& env) {
-      linkedits = env.archive.template subcommands<const LinkeditCommand<bits> *>();
-   }
-
-   template <Bits bits>
-   void Segment_LINKEDIT<bits>::Emit(Image& img, std::size_t offset) const {
-      /* find data offset of LC_SYMTAB, first linkedit entry */
-   }
-#endif
    
    template class Segment<Bits::M32>;
    template class Segment<Bits::M64>;
