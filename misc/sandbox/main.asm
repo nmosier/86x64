71737585466b0727bf75ff26cbcf676cb07445f5
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-05 12:21:42 -0700

    it wokrs!

--- misc/sandbox/main.asm
+++ misc/sandbox/main.asm
@@ -5,6 +5,7 @@
    extern _abort
    extern _memcpy
    extern _printf
+   extern _main2
 
 %define STACKSIZE 0x1000000
 %define PROT_READ 0x1
@@ -18,7 +19,10 @@
 _main:
    push rbp
    mov rbp, rsp
-   sub rsp, 16
+   sub rsp, 32
+   mov [rsp + 16], rdi          ; argc
+   mov [rsp + 24], rsi          ; argv
+   
    xor edi, edi
    mov rsi, STACKSIZE
    mov rdx, PROT_READ | PROT_WRITE
@@ -29,24 +33,43 @@ _main:
    cmp rax, MAP_FAILED
    je mmap_error
    
-   mov [rsp + 8], rax           ; save new stack pointer
+   mov [rsp + 0], rax           ; save new stack pointer
    mov rdi, rax
    add rdi, STACKSIZE - PAGESIZE
    mov rsi, rsp
    and rsi, ~PAGEMASK
+   mov rax, rdi
+   sub rax, rsi
+   mov [rsp + 8], rax           ; stack pointer difference
    mov rdx, PAGESIZE
    call _memcpy
 
-   mov rax, [rsp + 8]
+   ;; point rsp to new stack
+   mov rax, [rsp + 0]
    add rax, STACKSIZE - PAGESIZE
    and rsp, PAGEMASK
    add rsp, rax
 
+   ;; adjust argv
+   mov rax, [rsp + 8]
+   mov rdx, [rsp + 24]          ; argv
+   add rdx, rax                 ; new argv
+   mov [rsp + 24], rdx
+.argv_loop: 
+   add qword [rdx], rax
+   add rdx, 8
+   cmp qword [rdx], 0
+   jne .argv_loop
+
+   ;; success
    lea rdi, [rel success_msg]
    mov rsi, rsp
    call _printf
-   
-   ;; todo
+
+   ;; work
+   mov rdi, [rsp + 16]          ; argc
+   mov rsi, [rsp + 24]          ; argv
+   call _main2
    
    xor eax, eax
    leave
