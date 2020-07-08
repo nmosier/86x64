07cfae7c887d2ac53b78e2d08b68a23de9ee733c
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-08 13:02:18 -0700

    trie test

---
+++ c++/trie-test.cc
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+
+#include "trie.hh"
+
+int main(int argc, char *argv[]) {
+   trie<char, std::string> t;
+
+   if (argc > 1) {
+      for (int i = 1; i < argc; ++i) {
+         std::string arg(argv[i]);
+         t.insert(arg.begin(), arg.end());
+      }
+   } else {
+      std::string s;
+      while (std::cin >> s) {
+         t.insert(s);
+      }
+   }
+
+   for (auto it = t.begin(); it != t.end(); ++it) {
+      std::cout << *it << std::endl;
+   }
+   
+   return 0;
+}
