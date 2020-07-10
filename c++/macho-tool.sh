14594dd64b032de25d8b2120d07665b8c960bca1
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-10 08:57:13 -0700

    dfd

--- c++/macho-tool.sh
+++ c++/macho-tool.sh
@@ -1,6 +1,36 @@
 # complete -W "help noop modify translate tweak convert" macho-tool
+
+_macho_tool_completions_command() {
+    COMMAND=$1
+    ARG="${COMP_WORDS[${#COMP_WORDS[@]}-1]}"
+    
+    cmd_help=""
+    cmd_noop="--help"
+    cmd_modify="--help --insert --delete --start"
+    cmd_translate="--help --offset"
+    cmd_tweak="--help --flags"
+    cmd_convert="--help --archive"
+    
+    lookup=cmd_$COMMAND
+    options=${!lookup}
+    COMPREPLY=($(compgen -W "$options" -- "$ARG"))
+}
+
+
 _macho_tool_completions() {
-    COMPREPLY=(${COMP_WORDS[@]})
+    # find command
+    COMMAND="${COMP_WORDS[1]}"
+
+    # check if in command list
+    CMDS="help noop modify translate tweak convert"
+    for REF in $CMDS; do
+        if [[ $REF = "$COMMAND" ]]; then
+            _macho_tool_completions_command $COMMAND
+            return
+        fi
+    done
+
+    COMPREPLY=($(compgen -W "$CMDS" -- $COMMAND))
 }
 
 complete -F _macho_tool_completions macho-tool
