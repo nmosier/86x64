6c49fe1483538c2217430672748a17711f93a031
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-16 08:22:22 -0700

    migrated implementation of macho-tool translate

--- c++/macho-tool/translate.cc
+++ c++/macho-tool/translate.cc
@@ -5,40 +5,58 @@
 #include "translate.hh"
 #include "macho.hh"
 #include "archive.hh"
+#include "util.hh"
 
 int TranslateCommand::opthandler(int optchar) {
+   Functor *f;
+   
    switch (optchar) {
    case 'h':
       usage(std::cout);
       return 0;
    case 'o':
-      {
-         char *endptr;
-         offset = std::strtoul(optarg, &endptr, 0);
-         if (*optarg == '\0' || *endptr) {
-            throw std::string("invalid offset");
-         }
-      }
-      return 1;
-         
+      f = new Offset;
+      break;
    default: abort();
    }
+
+   int parsestat = f->parse(optarg);
+   op = std::unique_ptr<Functor>(f);
+   return parsestat;
 }
 
 int TranslateCommand::work() {
    MachO::MachO *macho = MachO::MachO::Parse(*img);
-   
-   auto archive = dynamic_cast<MachO::Archive<MachO::Bits::M64> *>(macho);
-   if (archive == nullptr) {
-      log("only 64-bit archives supported");
-      return -1;
-   }
-   
-   if (offset) {
-      std::cout << std::hex << "0x" << archive->offset_to_vmaddr(offset) << std::endl;
-   }
-   
+   (*op)(macho);
    return 0;
 }
 
 TranslateCommand::TranslateCommand(): InplaceCommand("translate", O_RDONLY) {}
+
+int TranslateCommand::Offset::parse(char *option) {
+   offset = stout<std::size_t>(option, nullptr, 0);
+   return 1;
+}
+
+void TranslateCommand::Offset::operator()(MachO::MachO *macho) {
+   switch (macho->bits()) {
+   case MachO::Bits::M32:
+      {
+         auto archive = dynamic_cast<MachO::Archive<MachO::Bits::M32> *>(macho);;
+         workT(archive);
+         break;
+      }
+   case MachO::Bits::M64:
+      {
+         auto archive = dynamic_cast<MachO::Archive<MachO::Bits::M64> *>(macho);;
+         workT(archive);
+         break;
+      }
+   default: abort();
+   }
+}
+
+template <MachO::Bits b>
+void TranslateCommand::Offset::workT(MachO::Archive<b> *archive) {
+   std::cout << std::hex << "0x" << archive->offset_to_vmaddr(*offset) << std::endl;   
+}
--- c++/macho-tool/translate.hh
+++ c++/macho-tool/translate.hh
@@ -1,20 +1,38 @@
 #pragma once
 
+#include <optional>
+
 #include "command.hh"
 
-struct TranslateCommand: public InplaceCommand {
-public:
+struct TranslateCommand: InplaceCommand {
+   struct Offset;
+   struct LoadDylib;
+   
+   std::unique_ptr<Functor> op;
+
    unsigned long offset = 0;
+
+   static constexpr int LOAD_DYLIB = 256;
 
    virtual std::string optusage() const override { return "[-h|-o <offset>]"; }
    virtual const char *optstring() const override { return "ho:"; }
    virtual std::vector<option> longopts() const override {
       return {{"help", no_argument, nullptr, 'h'},
               {"offset", required_argument, nullptr, 'o'},
+              {"load-dylib", required_argument, nullptr, LOAD_DYLIB},
               {0}};
    }
-
+   
    virtual int opthandler(int optchar) override;
    virtual int work() override;
    TranslateCommand();
+};
+
+struct TranslateCommand::Offset: Functor {
+   std::optional<std::size_t> offset;
+
+   virtual int parse(char *option) override;
+   virtual void operator()(MachO::MachO *macho) override;
+
+   template <MachO::Bits b> void workT(MachO::Archive<b> *archive);
 };
