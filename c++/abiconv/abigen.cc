f4c7e47ee8e4a1c3b99b106cf889ac83e2daed1a
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-18 13:08:27 -0700

    before signedness

--- c++/abiconv/abigen.cc
+++ c++/abiconv/abigen.cc
@@ -3,11 +3,13 @@
 #include <unordered_map>
 #include <unistd.h>
 #include <sstream>
-#include <forward_list>
+#include <list>
 
 #include "util.hh"
 
-enum class type_token {C, S, I, L, LL, P};
+enum class type_token {C, S, I, L, LL, P,
+                       UC, US, UI, UL, ULL,
+                       SC};
 
 static type_token str_to_token(const std::string& s) {
    const std::unordered_map<std::string, type_token> map =
@@ -17,6 +19,12 @@ static type_token str_to_token(const std::string& s) {
        {"l", type_token::L},
        {"ll", type_token::LL},
        {"p", type_token::P},
+       {"uc", type_token::UC},
+       {"us", type_token::US},
+       {"ui", type_token::UI},
+       {"ul", type_token::UL},
+       {"ull", type_token::ULL},
+       {"sc", type_token::SC},
       };
 
    std::string s_ = strmap(s, tolower);
@@ -29,10 +37,12 @@ static type_token str_to_token(const std::string& s) {
 
 struct signature {
    std::string sym;
-   std::forward_list<type_token> params;
+   std::list<type_token> params;
+
+   void Emit(std::ostream& os, const std::string& prefix);
 };
 
-std::istream& operator>>(std::istream& is, signature& sign) {
+static std::istream& operator>>(std::istream& is, signature& sign) {
    std::string line_tmp;
    std::getline(is, line_tmp);
    std::stringstream line(line_tmp);
@@ -43,10 +53,101 @@ std::istream& operator>>(std::istream& is, signature& sign) {
 
    std::string tokstr;
    while (line >> tokstr) {
-      sign.params.push_front(str_to_token(tokstr));
+      sign.params.push_back(str_to_token(tokstr));
    }
 
    return is;
+}
+
+template <typename Opcode>
+static std::ostream& emit_inst(std::ostream& os, Opcode&& opcode) {
+   return os << "\t" << opcode << std::endl;
+}
+
+template <typename Opcode, typename OperandHead, typename... OperandTail>
+static std::ostream& emit_inst(std::ostream& os, Opcode&& opcode, OperandHead&& head,
+                               OperandTail&&... tail) {
+   return strjoin(os << "\t" << opcode << "\t", ",\t", head, tail...) << std::endl;
+}
+
+enum class reg_width {B, W, D, Q};
+
+static reg_width get_token_width(type_token tok) {
+   switch (tok) {
+   case type_token::C:
+   case type_token::UC:
+   case type_token::SC:
+      return reg_width::B;
+   case type_token::S:
+   case type_token::US:
+      return reg_width::W;
+   case type_token::I:
+   case type_token::UI:
+   case type_token::L:
+   case type_token::UL:
+   case type_token::P:
+      return reg_width::D;
+   case type_token::LL:
+   case type_token::ULL:
+      return reg_width::Q;
+   default: throw std::invalid_argument("bad token");
+   }
+}
+
+struct reg_group {
+   const std::string reg_b;
+   const std::string reg_w;
+   const std::string reg_d;
+   const std::string reg_q;
+   
+   const std::string& reg(reg_width width) const {
+      switch (width) {
+      case reg_width::B: return reg_b;
+      case reg_width::W: return reg_w;
+      case reg_width::D: return reg_d;
+      case reg_width::Q: return reg_q;
+      default: throw std::invalid_argument("bad register width");
+      }
+   }
+};
+
+static const reg_group rdi = {"dil", "di",  "edi", "rdi"};
+static const reg_group rsi = {"sil", "si",  "esi", "rsi"};
+static const reg_group rdx = {"dl",  "dx",  "edx", "rdx"};
+static const reg_group rcx = {"cl",  "cx",  "ecx", "rcx"};
+static const reg_group r8  = {"r8b", "r8w", "r8d", "r8"};
+static const reg_group r9  = {"r9b", "r9w", "r9d", "r9"};
+
+void signature::Emit(std::ostream& os, const std::string& prefix) {
+   os << prefix << sym << ":" << std::endl;
+
+   /* save registers */
+   emit_inst(os, "push", "rbp");
+   emit_inst(os, "mov", "rbp", "rsp");
+   emit_inst(os, "push", "rdi");
+   emit_inst(os, "push", "rsi");
+
+   /* align stack */
+   emit_inst(os, "and", "rsp", "~0xf");
+
+   /* transfer arguments */
+   const std::list<const reg_group *> arg_regs =
+      {&rdi, &rsi, &rdx, &rcx, &r8, &r9};
+
+   // NOTE: Don't support ll's yet, so each arg has 4-byte alignment.
+   /*  ...
+    *  arg2
+    *  arg1
+    *  ret
+    *  caller rbp <-- callee rbp
+    */
+   std::size_t frame_offset;
+   for (auto param_it = params.rbegin(); param_it != params.rend(); ++param_it) {
+      
+   }
+   
+
+   
 }
 
 int main(int argc, char *argv[]) {
@@ -67,6 +168,7 @@ int main(int argc, char *argv[]) {
 
    signature sign;
    std::cin >> sign;
+   sign.Emit(std::cout, "__");
    
    // TODO
    
