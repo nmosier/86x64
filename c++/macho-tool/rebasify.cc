707f154bfea3e545bd02c50e985512d2273ae602
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-20 08:13:13 -0700

    added

--- c++/macho-tool/rebasify.cc
+++ c++/macho-tool/rebasify.cc
@@ -19,6 +19,13 @@ int Rebasify::opthandler(int optchar) {
    }
 }
 
+void Rebasify::state_info::reset() {
+   state = 0;
+   vmaddr = 0;
+   reg = XED_REG_EAX;
+   frame_index = -1;
+}
+
 int Rebasify::work() {
    MachO::MachO *macho = MachO::MachO::Parse(*in_img);
    auto archive32 = dynamic_cast<MachO::Archive<MachO::Bits::M32> *>(macho);
@@ -40,8 +47,11 @@ int Rebasify::work() {
       return -1;
    }
 
+#if 0   
    int state = 0;
-   std::size_t eax_vmaddr;
+   std::size_t reg_vmaddr;
+   xed_reg_enum_t thunk_reg;
+   int frame_offset;
    const MachO::opcode_t call_opcode = {0xe8, 0x00, 0x00, 0x00, 0x00};
    const MachO::opcode_t pop_opcode = {0x58};
    for (auto text_it = text->content.begin(); text_it != text->content.end(); ++text_it) {
@@ -52,142 +62,176 @@ int Rebasify::work() {
             state = 1;
          } else if (inst->instbuf == pop_opcode && state == 1) {
             state = 2;
-            eax_vmaddr = inst->loc.vmaddr;
-            // fprintf(stderr, "[REBASIFY] pop, eax = 0x%zx\n", eax_vmaddr);
+            reg_vmaddr = inst->loc.vmaddr;
+            thunk_reg = XED_REG_EAX;
+            frame_offset = -1;
          } else {
             state = 0;
          }
       } else {
-         /* check if reads from or writes to eax */
-         const xed_reg_enum_t reg0 = xed_decoded_inst_get_reg(&inst->xedd, XED_OPERAND_REG0);
-         const xed_reg_enum_t reg1 = xed_decoded_inst_get_reg(&inst->xedd, XED_OPERAND_REG1);
-         const unsigned nmemops = xed_decoded_inst_number_of_memory_operands(&inst->xedd);
-
-         switch (xed_decoded_inst_get_iclass(&inst->xedd)) {
-         case XED_ICLASS_PUSH: /* give up on tracking %eax */
-         case XED_ICLASS_POP: /* %eax overwritten */
-         case XED_ICLASS_CALL_FAR:
-         case XED_ICLASS_CALL_NEAR:
-         case XED_ICLASS_JMP:
-         case XED_ICLASS_RET_NEAR:
-         case XED_ICLASS_RET_FAR:
-         case XED_ICLASS_JB:
-         case XED_ICLASS_JBE: 
-         case XED_ICLASS_JCXZ: 
-         case XED_ICLASS_JECXZ: 
-         case XED_ICLASS_JL: 
-         case XED_ICLASS_JLE:
-         case XED_ICLASS_JMP_FAR: 
-         case XED_ICLASS_JNB: 
-         case XED_ICLASS_JNBE: 
-         case XED_ICLASS_JNL: 
-         case XED_ICLASS_JNLE: 
-         case XED_ICLASS_JNO: 
-         case XED_ICLASS_JNP: 
-         case XED_ICLASS_JNS: 
-         case XED_ICLASS_JNZ: 
-         case XED_ICLASS_JO: 
-         case XED_ICLASS_JP: 
-         case XED_ICLASS_JRCXZ: 
-         case XED_ICLASS_JS: 
-         case XED_ICLASS_JZ: 
-            state = 0;
-            break;
-
-#if 0
-         case XED_ICLASS_LEA:
-            {
-               const int64_t disp = xed_decoded_inst_get_memory_displacement(&inst->xedd, 0);
-               if (reg0 == XED_REG_EAX &&
-                   xed_decoded_inst_get_base_reg(&inst->xedd, 0) == XED_REG_EAX) {
-                  /* update value of eax_vmaddr */
-                  eax_vmaddr += disp;
-               } else if (reg0 == XED_REG_EAX) {
-                  state = 0; /* eax is overwritten */
-               } else if (reg1 == XED_REG_EAX) {
-                  /* eax used in address expression */
-                  const std::size_t vmaddr = eax_vmaddr + disp;
-
-                  /* find target */
-                  if ((memdisp = archive->template find_blob<SectionBlob>(vmaddr)) == nullptr) {
-                     fprintf(stderr, "warning: unable to find memdisp for thunk eax\n");
-                  }
-                  
-                  
-#warning ADD REBASE
-               }
-               // fprintf(stderr, "[REBASIFY] 0x%zx, lea = 0x%zx\n", inst->loc.vmaddr, eax_vmaddr);
-            }
-            break;
-#endif
-               
-         default:
-            {
-               std::size_t target = eax_vmaddr;
-                  
-               /* use index */
-               unsigned memidx;
-               for (memidx = 0; memidx < nmemops; ++memidx) {
-                  if (xed_decoded_inst_get_base_reg(&inst->xedd, memidx) == XED_REG_EAX) {
-                     const int64_t disp =
-                        xed_decoded_inst_get_memory_displacement(&inst->xedd, memidx);
-                     target += disp;
-                     break;
-                  }
-               }
-
-               if (memidx != nmemops || reg1 == XED_REG_EAX) {
-                  auto mov_inst = new MachO::Instruction<MachO::Bits::M32>(MachO::opcode::mov_eax_imm32());
-                  auto mov_inst_imm = MachO::Immediate<MachO::Bits::M32>::Create(target);
-                  if ((mov_inst->segment = mov_inst_imm->segment = archive32->segment(SEG_TEXT)) == nullptr) {
-                     log("missing TEXT segment");
-                     return -1;
-                  }
-                  if ((mov_inst->section = mov_inst_imm->section = archive32->section(SECT_TEXT)) == nullptr) {
-                     log("missing text section");
-                     return -1;
-                  }
-                  if ((mov_inst_imm->pointee = archive32->find_blob<MachO::SectionBlob>(target)) == nullptr) {
-                     log("unable to find destination blob in rebasify operation at vmaddr 0x%zx",
-                         inst->loc.vmaddr);
-                     return -1;
-                  }
-
-                  mov_inst->imm = mov_inst_imm;
-
-                  text->content.insert(text_it, mov_inst);
-
-                  /* adjust displacement if necessary */
-                  if (memidx < nmemops) {
-                     const xed_enc_displacement_t encdisp =
-                        {0, xed_decoded_inst_get_memory_displacement_width_bits(&inst->xedd, memidx)};
-                     if (!xed_patch_disp(&inst->xedd, inst->instbuf.data(), encdisp)) {
-                        log("error while patching displacement");
-                        return -1;
-                     }
-                  }
-
-                  /* add to rebase info */
-                  auto rebase_node = MachO::RebaseNode<MachO::Bits::M32>::Create(REBASE_TYPE_TEXT_ABSOLUTE32);
-                  rebase_node->blob = mov_inst_imm;
-                  auto dyld_info = archive32->template subcommand<MachO::DyldInfo>();
-                  if (dyld_info == nullptr) {
-                     log("missing dyld info");
-                     return -1;
-                  }
-                  auto rebase_info = dyld_info->rebase;
-                  rebase_info->rebasees.push_back(rebase_node);
-                  
-                  
-                  // fprintf(stderr, "[REBASIFY] 0x%zx, target = 0x%zx\n", inst->loc.vmaddr, target);
-               }
-               break;
-            }
-         }
       }
    }
+#else
+   state_info state(archive32);
+   for (auto text_it = state.section->content.begin(); text_it != state.section->content.end();
+        ++text_it) {
+      auto text_inst = dynamic_cast<MachO::Instruction<MachO::Bits::M32> *>(*text_it);
+      if (text_inst) {
+         decode_info info(text_inst->xedd, text_it);
+         handle_inst(text_inst, state, info);
+      }
+   }
+#endif
       
    archive32->Build(0);
    archive32->Emit(*out_img);
    return 0;
+}
+
+Rebasify::decode_info::decode_info(const xed_decoded_inst_t& xedd,
+                                   typename MachO::Section<MachO::Bits::M32>::Content::iterator it):
+   text_it(it),
+   reg0(xed_decoded_inst_get_reg(&xedd, XED_OPERAND_REG0)),
+   reg1(xed_decoded_inst_get_reg(&xedd, XED_OPERAND_REG1)),
+   iform(xed_decoded_inst_get_iform_enum(&xedd)),
+   iclass(xed_decoded_inst_get_iclass(&xedd)),
+   nmemops(xed_decoded_inst_number_of_memory_operands(&xedd)),
+   base_reg(xed_decoded_inst_get_base_reg(&xedd, 0)),
+   memdisp(xed_decoded_inst_get_memory_displacement(&xedd, 0)) {}
+
+int Rebasify::handle_inst(MachO::Instruction<MachO::Bits::M32> *inst, state_info& state,
+                           const decode_info& info) const {
+   switch (state.state) {
+   case 0: /* init */
+      if (inst->instbuf == MachO::opcode_t({0xe8, 0x00, 0x00, 0x00, 0x00})) {
+         state.state = 1;
+      }
+      return 0;
+      
+   case 1: /* seen call */
+      if (inst->instbuf == MachO::opcode_t({0x58})) {
+         state.state = 2;
+         state.reg = XED_REG_EAX;
+         state.vmaddr = inst->loc.vmaddr;
+      }
+      return 0;
+
+   case 2: /* seen pop */
+      {
+         if ((info.iform == XED_IFORM_MOV_MEMv_OrAX || info.iform == XED_IFORM_MOV_MEMv_GPRv) &&
+             info.reg0 == state.reg && info.base_reg == XED_REG_EBP) {
+            /* frame store */
+            state.state = 3;
+         } else {
+            handle_inst_thunk(inst, state, info);
+         }
+         return 0;
+      }
+
+   case 3: /* seen frame store */
+      {
+         if ((info.iform == XED_IFORM_MOV_GPRv_MEMv || info.iform == XED_IFORM_MOV_OrAX_MEMv) &&
+             info.base_reg == XED_REG_EBP && info.memdisp == state.frame_index) {
+            state.reg = info.reg0;
+            state.state = 2;
+         }
+         return 0;
+      }
+      
+      // TODO
+   default: abort();
+   }
+}
+
+int Rebasify::handle_inst_thunk(MachO::Instruction<MachO::Bits::M32> *inst, state_info& state,
+                                 const decode_info& info) const {
+   /* check if reads from or writes to eax */
+   switch (xed_decoded_inst_get_iclass(&inst->xedd)) {
+   case XED_ICLASS_PUSH: /* give up on tracking %eax */
+   case XED_ICLASS_POP: /* %eax overwritten */
+   case XED_ICLASS_CALL_FAR:
+   case XED_ICLASS_CALL_NEAR:
+   case XED_ICLASS_JMP:
+   case XED_ICLASS_RET_NEAR:
+   case XED_ICLASS_RET_FAR:
+   case XED_ICLASS_JB:
+   case XED_ICLASS_JBE: 
+   case XED_ICLASS_JCXZ: 
+   case XED_ICLASS_JECXZ: 
+   case XED_ICLASS_JL: 
+   case XED_ICLASS_JLE:
+   case XED_ICLASS_JMP_FAR: 
+   case XED_ICLASS_JNB: 
+   case XED_ICLASS_JNBE: 
+   case XED_ICLASS_JNL: 
+   case XED_ICLASS_JNLE: 
+   case XED_ICLASS_JNO: 
+   case XED_ICLASS_JNP: 
+   case XED_ICLASS_JNS: 
+   case XED_ICLASS_JNZ: 
+   case XED_ICLASS_JO: 
+   case XED_ICLASS_JP: 
+   case XED_ICLASS_JRCXZ: 
+   case XED_ICLASS_JS: 
+   case XED_ICLASS_JZ: 
+      state.state = 0;
+      break;
+
+   default:
+      {
+         std::size_t target = state.vmaddr;
+         if (info.base_reg == state.reg) {
+            target += info.memdisp;
+         }
+                  
+         if (info.base_reg == state.reg || info.reg1 == state.reg) {
+            auto mov_inst = new MachO::Instruction<MachO::Bits::M32>
+               (MachO::opcode::mov_eax_imm32());
+            auto mov_inst_imm = MachO::Immediate<MachO::Bits::M32>::Create(target);
+            mov_inst->segment = mov_inst_imm->segment = state.segment;
+            mov_inst->section = mov_inst_imm->section = state.section;
+
+            if ((mov_inst_imm->pointee =
+                 state.archive->template find_blob<MachO::SectionBlob>(target)) == nullptr) {
+               log("unable to find destination blob in rebasify operation at vmaddr 0x%zx",
+                   inst->loc.vmaddr);
+               return -1;
+            }
+                     
+            mov_inst->imm = mov_inst_imm;
+
+            state.section->content.insert(info.text_it, mov_inst);
+                     
+            /* adjust displacement if necessary */
+            if (info.base_reg == state.reg) {
+               const xed_enc_displacement_t encdisp =
+                  {0, xed_decoded_inst_get_memory_displacement_width_bits(&inst->xedd, 0)};
+               if (!xed_patch_disp(&inst->xedd, inst->instbuf.data(), encdisp)) {
+                  log("error while patching displacement");
+                  return -1;
+               }
+            }
+                     
+            /* add to rebase info */
+            auto rebase_node = MachO::RebaseNode<MachO::Bits::M32>::
+               Create(REBASE_TYPE_TEXT_ABSOLUTE32);
+            rebase_node->blob = mov_inst_imm;
+            auto dyld_info = state.archive->template subcommand<MachO::DyldInfo>();
+            if (dyld_info == nullptr) {
+               log("missing dyld info");
+               return -1;
+            }
+            auto rebase_info = dyld_info->rebase;
+            rebase_info->rebasees.push_back(rebase_node);
+         }
+         break;
+      }
+   }
+
+   return 0;
+}
+
+Rebasify::state_info::state_info(MachO::Archive<MachO::Bits::M32> *archive):
+   archive(archive), segment(archive->segment(SEG_TEXT)), section(archive->section(SECT_TEXT)) {
+   reset();
 }
--- c++/macho-tool/rebasify.hh
+++ c++/macho-tool/rebasify.hh
@@ -1,8 +1,44 @@
 #pragma once
 
+extern "C" {
+#include <xed-interface.h>
+}
 #include "command.hh"
+#include "segment.hh"
+#include "section.hh"
 
 struct Rebasify: InOutCommand {
+   struct state_info {
+      /* 0 - init
+       * 1 - seen 'call 0'
+       * 2 - seen 'pop eax'
+       * 3 - seen 'mov [ebp - idx], reg'
+       */
+      int state;
+      std::size_t vmaddr;
+      xed_reg_enum_t reg;
+      int frame_index;
+
+      MachO::Archive<MachO::Bits::M32> *archive = nullptr;
+      MachO::Segment<MachO::Bits::M32> *segment = nullptr;
+      MachO::Section<MachO::Bits::M32> *section = nullptr;
+      
+      void reset();
+      state_info(MachO::Archive<MachO::Bits::M32> *archive);
+   };
+
+   struct decode_info {
+      typename MachO::Section<MachO::Bits::M32>::Content::iterator text_it;
+      xed_reg_enum_t reg0, reg1;
+      xed_iform_enum_t iform;
+      xed_iclass_enum_t iclass;
+      unsigned nmemops;
+      xed_reg_enum_t base_reg;
+      int64_t memdisp;
+
+      decode_info(const xed_decoded_inst_t& xedd,
+                  typename MachO::Section<MachO::Bits::M32>::Content::iterator text_it);
+   };
 
    virtual const char *optstring() const override { return "h"; }
    virtual std::vector<option> longopts() const override {
@@ -12,6 +48,10 @@ struct Rebasify: InOutCommand {
    virtual int opthandler(int optchar) override;
    virtual std::string optusage() const override { return "[-h]"; }
    virtual int work() override;
-   void add_rebase(std::size_t vmaddr, uint8_t type);
    Rebasify(): InOutCommand("rebasify") {}
+
+   int handle_inst(MachO::Instruction<MachO::Bits::M32> *inst, state_info& state,
+                   const decode_info& info) const;
+   int handle_inst_thunk(MachO::Instruction<MachO::Bits::M32> *inst, state_info& state,
+                         const decode_info& info) const;
 };
