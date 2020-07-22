889652e89c30e16e579a960780624893d5527013
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-22 09:08:20 -0700

    maybe fixed' g

--- extern/build-xed.sh
+++ extern/build-xed.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then
-    echo "usage: $0 <xed-src-path> <xed-build-path>"
+if [ $# -ne 3 ]; then
+    echo "usage: $0 <xed-src-path> <xed-build-path> <log-path>"
     exit 1;
 fi
 
 SOURCE="$1"
 BUILD="$2"
+LOG="$3"
 
 if ! [ -r "$BUILD/lib/libxed.a" -a -r "$BUILD/include/xed/xed-interface.h" ]; then
-    "$SOURCE/xed/src/mfile.py" --install-dir="$BUILD" --jobs=8 -s install > "$BUILD/xed/src/xed-stamp/build.log"
+    "$SOURCE/xed/src/mfile.py" --install-dir="$BUILD" --jobs=8 -s install > "$LOG"
 fi
