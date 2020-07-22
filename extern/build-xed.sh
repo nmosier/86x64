500d789d1475785f4e116b782b2ceaff7fc70ae7
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-22 09:28:19 -0700

    ds " gi

--- extern/build-xed.sh
+++ extern/build-xed.sh
@@ -7,9 +7,8 @@ fi
 
 SOURCE="$1"
 BUILD="$2"
-LOG="$3"
 
 if ! [ -r "$BUILD/lib/libxed.a" -a -r "$BUILD/include/xed/xed-interface.h" ]; then
-    [ -d "$BUILD/obj" ] || mkdir "$BUILD/obj"
-    "$SOURCE/mfile.py" --install-dir="$BUILD" --jobs=8 -s install
+    cd "$BUILD"
+    "$SOURCE/mfile.py" --jobs=8 -s
 fi
