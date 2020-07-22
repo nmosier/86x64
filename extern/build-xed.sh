9a084627c2c613f2072750ba6239d51d992a0d1e
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-22 09:10:02 -0700

    commit

--- extern/build-xed.sh
+++ extern/build-xed.sh
@@ -10,5 +10,5 @@ BUILD="$2"
 LOG="$3"
 
 if ! [ -r "$BUILD/lib/libxed.a" -a -r "$BUILD/include/xed/xed-interface.h" ]; then
-    "$SOURCE/xed/src/mfile.py" --install-dir="$BUILD" --jobs=8 -s install > "$LOG"
+    "$SOURCE/mfile.py" --install-dir="$BUILD" --jobs=8 -s install > "$LOG"
 fi
