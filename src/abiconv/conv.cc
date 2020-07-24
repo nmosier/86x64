6abd121e68928a081f4c017baf48bb95655fc02a
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-24 14:56:15 -0700

    arg counts

--- src/abiconv/conv.cc
+++ src/abiconv/conv.cc
@@ -28,16 +28,18 @@ namespace {
    }
 
    template <typename T32, typename T64>
-   T64 convert_arg(const void *& args32, void *& args64, reg_width_t *& argtypes) {
+   inline T64 convert_arg(const void *& args32, void *& args64, reg_width_t *& argtypes,
+                          unsigned& arg_count) {
       const T64 result = * (T64 *) args64 = * (T32 *) args32;
       args32 = (const char *) args32 + align_up<size_t>(sizeof(T32), 4);
       args64 = (char *) args64 + align_up<size_t>(sizeof(T64), 8);
       *argtypes++ = type_reg_width<T64>();
+      ++arg_count;
       return result;
    }
 
    void printf_parse_directive(const void *& args32, void *& args64, reg_width_t *& argtypes,
-                               const char *format) {
+                               const char *& format, unsigned& arg_count) {
       switch (*format++) {
       case 'd':
       case 'i':
@@ -46,18 +48,18 @@ namespace {
       case 'x':
       case 'X':
       case 'c':
-         convert_arg<int32_t, int32_t>(args32, args64, argtypes);
+         convert_arg<int32_t, int32_t>(args32, args64, argtypes, arg_count);
          break;
 
       case 'D':
       case 'O':
       case 'U':
-         convert_arg<int32_t, int64_t>(args32, args64, argtypes);
+         convert_arg<int32_t, int64_t>(args32, args64, argtypes, arg_count);
          break;
 
       case 's':
       case 'p':
-         convert_arg<ptr32_t, ptr64_t>(args32, args64, argtypes);
+         convert_arg<ptr32_t, ptr64_t>(args32, args64, argtypes, arg_count);
          break;
 
       case '%':
@@ -70,24 +72,24 @@ namespace {
    
 }
 
-extern "C" int printf_conversion_f(const void *args32, void *args64, reg_width_t *argtypes) {
+extern "C" unsigned printf_conversion_f(const void *args32, void *args64, reg_width_t *argtypes) {
    unsigned idx_64 = 0;
    unsigned idx_32 = 0;
-   
-   const char *format = (const char *) convert_arg<ptr32_t, ptr64_t>(args32, args64, argtypes);
+   unsigned arg_count = 0;
 
+   const char *format = (const char *) convert_arg<ptr32_t, ptr64_t>(args32, args64, argtypes,
+                                                                     arg_count);
    char c;
    while ((c = *format++)) {
       switch (c) {
       case '%':
-         printf_parse_directive(args32, args64, argtypes, format);
+         printf_parse_directive(args32, args64, argtypes, format, arg_count);
          break;
          
       default:
          break;
       }
    }
-
-   return 0;
    
+   return 0;
 }
