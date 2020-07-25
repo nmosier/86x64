a85e1c1a9c8014f2f941d03ffe70b27171d8dabe
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-25 15:28:19 -0700

    tests are working

--- tests/do-test.sh
+++ tests/do-test.sh
@@ -2,16 +2,16 @@
 
 usage() {
     echo <<EOF
-usage: $0 exec32 exec64
+usage: $0 exec
 EOF
 }
 
-if [[ $# -ne 2 ]]; then
+if [[ $# -ne 1 ]]; then
     usage >&2
     exit 1
 fi
 
-if ! [ -x "$1" ] || ! [ -x "$2" ]; then
+if ! [ -x "${1}32" ] || ! [ -x "${1}64" ]; then
     echo "file not found" >&2
     exit 1
 fi
@@ -20,8 +20,12 @@ tmp1=$(mktemp)
 tmp2=$(mktemp)
 trap "rm -rf $tmp1 $tmp2" EXIT
 
-if ! "$1" > "$tmp1" || ! sudo chroot / sh -c "cd \"$(dirname "$2")\"; \"$2\"" > "$tmp2"; then
+if ! "${1}32" > "$tmp1" || ! sudo chroot / sh -c "cd \"$(dirname "${1}64")\"; \"${1}64\"" > "$tmp2"; then
     exit 1
 fi
 
-diff "$tmp1" "$tmp2"
+if [ -e "$1.sh" ]; then
+    diff <("$1.sh" "$1") "$tmp2"
+else
+    diff "$tmp1" "$tmp2"
+fi
---
+++ tests/printf-many.sh
@@ -0,0 +1,1 @@
+printf 'argc=%d, argv[0]=%s\n' 1 "${1}64"
