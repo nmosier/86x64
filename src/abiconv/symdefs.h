bff9408cae5b53d9d2ec3f7835052df02450b5f5
Nicholas Mosier <nh.mosier@gmail.com>
2020-07-28 09:31:54 -0700

    rearranged some

--- src/abiconv/symdefs.h
+++
@@ -1,6 +0,0 @@
-void exit(int);
-int puts(const char *);
-int fputs(const char *, void *);
-int getopt(int argc, char * const argv[], const char *optstring);
-DIR *opendir(const char *filename);
-// int fputs$UNIX2003(const char *, void *);
